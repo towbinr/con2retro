d699ee454e2707bdd2317289500a998ee273a1a6
towbinr <44979218+towbinr@users.noreply.github.com>
2018-12-23 17:45:56 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1545603522,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/spritefont.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite12-sheet0.png",
+		"images/spritefont4.png",
+		"media/jump_07.m4a",
+		"media/jump_07.ogg",
+		"media/hit_00.m4a",
+		"media/hit_00.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
